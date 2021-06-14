966721c21a36e38c57a7b6ad94dd0183bf481c9c
elenagiura <elenagiura94@gmail.com>
2021-06-14 12:30:21 +0200

    Live site

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ const App = () => {
   return (
     <React.Fragment>
       <Switch>
-        <Route exact path='/home'>
+        <Route exact path='/'>
           <Search toWatch={toWatch} movieList={movieList}/>
           <Movies watched={false} movieList={moviesForWatch} hide={hide} remove={remove} move={move} filterMovies={filterMovies} filters={filters} reset={()=>getResults()} info={info}/>
           <Movies watched={true} movieList={watchedMovies} hide={hide} remove={remove} move={move} filterMovies={filterMovies} filters={filters} reset={()=>getResults()} info={info}/>
